5acd2104442316bf8dff17e9c19bb302cd6a3968
tehforsch <mail@tonipeter.de>
2023-11-02 22:35:51 +0100

    add melody, almost fix audio playback

--- flash.sh
+++ flash.sh
@@ -1,3 +1,16 @@
-sudo mkdir /run/media/RPI-RP2
-sudo mount /dev/sdb1 /run/media/RPI-RP2
-crr
+id=RPI-RP2
+echo $id
+mountPath=/run/media/toni/$id
+devPart=$(sudo blkid | grep $id | cut -d ":" -f 1)
+if [[ $devPart == "" ]]; then
+    echo "partition not found"
+    exit 1
+fi
+echo "mounting $devPart"
+echo mkdir -p $mountPath
+sudo mkdir -p $mountPath
+sudo umount $mountPath
+sudo mount $devPart $mountPath
+sudo chmod ugo+rwx $mountPath
+sudo chmod -R ugo+rwx $mountPath/
+cargo run --release
--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,15 @@
 
 mod tone;
 
+use rp_pico as bsp;
+
 use bsp::{
     entry,
-    hal::pwm::{InputHighRunning, Slices},
+    hal::{self},
 };
 use defmt_rtt as _;
-use embedded_hal::{digital::v2::OutputPin, PwmPin};
+use embedded_hal::PwmPin;
 use panic_probe as _;
-
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
@@ -35,10 +32,8 @@ fn main() -> ! {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+        XTAL_FREQ_HZ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -51,51 +46,28 @@ fn main() -> ! {
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins = bsp::Pins::new(
+    let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead. If you have
-    // a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here.
-    let mut led_pin = pins.led.into_push_pull_output();
-
-    // Init PWMs
-    let pwm_slices = Slices::new(pac.PWM, &mut pac.RESETS);
-
-    // Configure PWM4
-    let mut pwm = pwm_slices.pwm0;
+    let mut pwm_slices = hal::pwm::Slices::new(pac.PWM, &mut pac.RESETS);
+    let pwm = &mut pwm_slices.pwm0;
     pwm.set_ph_correct();
     pwm.enable();
 
-    // Set to run when b channel is high
-    let mut pwm = pwm.into_mode::<InputHighRunning>();
-    pwm.channel_b.input_from(pins.gpio1);
+    pwm.channel_b.output_to(pins.gpio1);
+    pwm.set_div_int(80);
 
-    let twinkle_twinkle = [
-        C4, C4, G4, G4, A4, A4, G4, NO_NOTE, F4, F4, E4, E4, D4, D4, C4, NO_NOTE, G4, G4, F4, F4,
-        E4, E4, D4, NO_NOTE, G4, G4, F4, F4, E4, E4, D4, NO_NOTE, C4, C4, G4, G4, A4, A4, G4,
-        NO_NOTE, F4, F4, E4, E4, D4, D4, C4, NO_NOTE,
-    ];
-
-    for top in twinkle_twinkle {
-        pwm.channel_b.set_duty(top / 2); // 50% Duty Cycle
-        pwm.set_top(top);
-        delay.delay_ms(500);
-
-        pwm.channel_b.set_duty(0);
-        delay.delay_ms(500);
-    }
-
+    let melody = my_favorite_melody();
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        for note in melody.iter() {
+            note.playback(pwm, &mut delay);
+
+            pwm.channel_b.set_duty(0);
+            delay.delay_ms(300);
+        }
     }
 }
